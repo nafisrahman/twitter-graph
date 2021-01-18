58e008238d3d1745e2768ae14d17233382bb381d
nafisrahman <nafisrahman797@gmail.com>
2021-01-17 22:10:48 -0500

    added lambda

---
+++ src/lambda.js
@@ -0,0 +1,38 @@
+exports.handler = (event, context, callback) => {
+  var Twitter = require('twitter')
+  require('dotenv/config')
+
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN,
+  })
+
+  // Jae78613139
+
+  var params = {
+    screen_name: event['queryStringParameters']['username'],
+    count: 200,
+    include_rts: true,
+  }
+
+  client.get(
+    'statuses/user_timeline',
+    params,
+    function (error, tweets, response) {
+      if (!error) {
+        let response1 = {
+          statusCode: 200,
+          isBase64Encoded: false,
+          headers: {
+            'x-custom-header': 'my custom header value',
+          },
+          body: JSON.stringify(tweets),
+        }
+        callback(null, response1)
+      } else {
+        console.log(error)
+      }
+    }
+  )
+}
